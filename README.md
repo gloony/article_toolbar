b4bdf7c872d98cbe1aa8fffbbbae14ed09aae27f
Emanuel Wontorra <emanuel.wontorra@wontorra.net>
2019-09-08 17:15:47 +0200

    removed unsused files, update new calls

--- README.md
+++ README.md
@@ -2,21 +2,21 @@ Article Toolbar
 =============
 
 Toolbar for easy access to feed functions
-https://github.com/idoxlr8/article_toolbar
+https://github.com/emanuelwo/article_toolbar
 
 
 
 Licensed under GNU GPL version 2
 
-Copyright (c) 2013 Tim James (unless explicitly stated otherwise).
+Copyright (c) 2019 Emanuel Wontorra (unless explicitly stated otherwise).
 
 ## Requirements
 
-* tt-rss v1.7.6 or be working from trunk at:
-https://github.com/gothfox/Tiny-Tiny-RSS
+* tt-rss v19.8 or be working from trunk at:
+https://git.tt-rss.org/fox/tt-rss
 
 Install:
-Unzip folder and drop in tt-rss plugins folder
+Unzip folder and drop in tt-rss plugins.local folder
 Enable plugin in preferences
 		
 
--- init.php
+++ init.php
@@ -4,9 +4,9 @@ class article_toolbar extends Plugin {
 	private $host;
 
 	function about() {
-		return array(1.1,
-			"Toolbar for easy access to feed functions",
-			"idoxlr8", false);
+		return array(1.0,
+			"Adds toolbar buttons to toggle sidebar",
+			"manu");
 	}
 
 	function init($host) {
@@ -16,16 +16,35 @@ class article_toolbar extends Plugin {
 	}
 
 	function get_js() {
-		return file_get_contents(dirname(__FILE__) . "/toolbar.js");
+		return '';
 	}
 
-	function HOOK_TOOLBAR_BUTTON() {
-		require_once dirname(__FILE__) . "/toolbar_body.php";
-	}
-  function api_version() {
-    return 2;
-  } 
+	function hook_toolbar_button() {
+		?>
+		<button dojoType="dijit.form.Button" onclick="Feeds.catchupAll()">
+			<i class="material-icons"
+               title="Mark all read">&nbsp;&#10003;&nbsp;</i>
+		</button>
 
+		<button dojoType="dijit.form.Button" onclick="Feeds.reloadCurrent()">
+			<i class="material-icons"
+               title="Refresh Feed">&nbsp;&#8635;&nbsp;</i>
+		</button>
+		<button dojoType="dijit.form.Button" onclick="Headlines.move('prev', true)">
+			<i class="material-icons"
+               title="Previous Post">&nbsp;&#9650;&nbsp;</i>
+		</button>
+		<button dojoType="dijit.form.Button" onclick="Headlines.move('next', true)">
+			<i class="material-icons"
+               title="Next Post">&nbsp;&#9660;&nbsp;</i>
+		</button>
+
+		<?php
+	}
+
+	function api_version() {
+		return 2;
+	}
 
 }
 ?>
